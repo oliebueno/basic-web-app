3c5341204d01b4c78cc0ae1a7ee707fe656c67f8
oliebueno <ollibueno08@gmail.com>
2024-11-18 14:32:36 -0400

    Agregado pregunta para el nombre

--- __tests__/utils/QueryProcessor.test.ts
+++ __tests__/utils/QueryProcessor.test.ts
@@ -1,37 +1,33 @@
 import QueryProcessor from "../../utils/QueryProcessor";
-import '@testing-library/jest-dom'
+import '@testing-library/jest-dom';
 
 describe("QueryProcessor", () => {
     test("should return a string", () => {
         const query = "test";
-        const response: string = QueryProcessor(query);
+        const response = QueryProcessor(query);
         expect(typeof response).toBe("string");
     });
 
     test('should return shakespeare description', () => {
         const query = "shakespeare";
-        const response: string = QueryProcessor(query);
-        expect(response).toBe((
+        const response = QueryProcessor(query);
+        expect(response).toBe(
             "William Shakespeare (26 April 1564 - 23 April 1616) was an " +
             "English poet, playwright, and actor, widely regarded as the greatest " +
             "writer in the English language and the world's pre-eminent dramatist."
-          ));
+        );
     });
 
-    // TODO: Deberías actualizar la prueba a continuación después de añadir tu USB ID
+    // Actualiza tu USB ID a continuación
     test('should return my USB ID', () => {
         const query = "what's your USB ID?";
-        const response: string = QueryProcessor(query);
-        expect(response).toBe((
-		"15-10192"
-    	));
+        const response = QueryProcessor(query);
+        expect(response).toBe("15-10192");
     });
 
     test('should return my name', () => {
         const query = "What is your name?";
-        const response: string = QueryProcessor(query);
-        expect(response).toBe((
-        "Oliver"
-        ));
+        const response = QueryProcessor(query);
+        expect(response).toBe("Oliver");
     });
 });
